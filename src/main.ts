0c71c4837b4bfce34a0719ae3d36a7652c305a38
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-10-15 09:13:52 -0400

    refactor: update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+
+  const config = new DocumentBuilder()
+    .setTitle('Bidding Process Extractor')
+    .setDescription('The Bidding Process Extractor API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
